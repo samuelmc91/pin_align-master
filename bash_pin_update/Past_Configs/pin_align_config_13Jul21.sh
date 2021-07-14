45303ae4e6e8dcee967de0edb880f901dae81feb
Samuel Clark <samuelmc91@yahoo.com>
2021-07-14 16:06:09 -0400

    Update to V3 w/ Auto Config

---
+++ bash_pin_update/Past_Configs/pin_align_config_13Jul21.sh
@@ -0,0 +1,120 @@
+#/bin/bash
+
+###########################################################
+
+DEFAULT_PIXELS_PER_MM="20"
+
+export X_POS=1
+export Y_POS=1
+export Z_POS
+
+X_CENTER=$((409))
+
+Y_CENTER=$((487))
+
+#########################################################
+
+BIG_BOX_X1=$((369))
+BIG_BOX_X2=$((664))
+
+BIG_BOX_Y1=$((362))
+BIG_BOX_Y2=$((612))
+
+PIN_ALIGN_ROI_WIDTH=$(((${BIG_BOX_X2}-${BIG_BOX_X1}) / 3))
+PIN_ALIGN_ROI_HEIGHT=$((${BIG_BOX_Y2}-${BIG_BOX_Y1}))
+
+########################## pin,body,base ##########################
+
+DEFAULT_ROI_Y1=$((362))
+
+PIN_TIP_X1=$((369))
+
+PIN_TIP="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_TIP_X1}+${DEFAULT_ROI_Y1}"
+
+PIN_BODY_X1=$((467))
+
+PIN_BODY="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_BODY_X1}+${DEFAULT_ROI_Y1}"
+
+PIN_BASE_X1=$((565))
+
+PIN_BASE="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_BASE_X1}+${DEFAULT_ROI_Y1}"
+
+########################## Tilt check  parameters ##########################
+
+TILT_CHECK_X1=$((614))
+TILT_CHECK_X2=$((664))
+
+TILT_CHECK_ROI_WIDTH=$((${TILT_CHECK_X2}-${TILT_CHECK_X1}))
+
+# Tilt check top
+TILT_CHECK_TOP_Y1=$((382))
+TILT_CHECK_TOP_Y2=$((452))
+
+TILT_CHECK_TOP_HEIGHT=$((${TILT_CHECK_TOP_Y2}-${TILT_CHECK_TOP_Y1}))
+
+TILT_CHECK_TOP="${TILT_CHECK_ROI_WIDTH}x${TILT_CHECK_TOP_HEIGHT}+${TILT_CHECK_X1}+${TILT_CHECK_TOP_Y1}"
+
+# Tilt check bottom
+TILT_CHECK_BOTTOM_Y1=$((522))
+TILT_CHECK_BOTTOM_Y2=$((592))
+
+TILT_CHECK_BOTTOM_HEIGHT=$((${TILT_CHECK_BOTTOM_Y2}-${TILT_CHECK_BOTTOM_Y1}))
+
+TILT_CHECK_BOTTOM="${TILT_CHECK_ROI_WIDTH}x${TILT_CHECK_TOP_HEIGHT}+${TILT_CHECK_X1}+${TILT_CHECK_BOTTOM_Y1}"
+
+########################## Pin check parameters ##########################
+
+PIN_CHECK_ROI_WIDTH=${PIN_ALIGN_ROI_WIDTH}
+
+PIN_CHECK_X1=${PIN_BODY_X1}
+
+PIN_CHECK_TOP_Y1=$((362))
+PIN_CHECK_TOP_Y2=$((452))
+
+PIN_CHECK_TOP_HEIGHT=$((${PIN_CHECK_TOP_Y2}-${PIN_CHECK_TOP_Y1}))
+
+PIN_CHECK_TOP="${PIN_CHECK_ROI_WIDTH}x${PIN_CHECK_TOP_HEIGHT}+${PIN_CHECK_X1}+${PIN_CHECK_TOP_Y1}"
+
+PIN_CHECK_BOTTOM_Y1=$((362))
+PIN_CHECK_BOTTOM_Y2=$((452))
+
+PIN_CHECK_BOTTOM_HEIGHT=$((${PIN_CHECK_BOTTOM_Y2}-${PIN_CHECK_BOTTOM_Y1}))
+
+PIN_CHECK_BOTTOM="${PIN_CHECK_ROI_WIDTH}x${PIN_CHECK_TOP_HEIGHT}+${PIN_CHECK_X1}+${PIN_CHECK_BOTTOM_Y1}"
+
+########################## X,Y,Z check parameters ##########################
+MIN_X=$((-2))
+MAX_X=$((2))
+
+MIN_Y=$((-2))
+MAX_Y=$((2))
+
+MIN_Z=$((-2))
+MAX_Z=$((2))
+
+##############################################################
+#  *** UNCOMMENT THE FOLLOWING LINE TO ENABLE DEBUG MODE ***
+# export PIN_ALIGN_DEBUG="yes"
+##############################################################
+
+# if [ "xx${PIN_ALIGN_DEBUG}" != "xx" ]; then
+#     echo "PIN_ALIGN: DEBUG enabled" 1>&2
+# fi
+# if [ "xx${PIN_ALIGN_DEBUG}" == "xx" ]; then
+#     echo "PIN_ALIGN: DEBUG disabled" 1>&2
+# fi
+
+# if [ "xx${PIN_ALIGN_DEBUG}" != "xx" ]; then
+#     if [ "xx${PIN_ALIGN_ROI_WIDTH}" != "xx" ]; then
+#         echo "PIN_ALIGN_ROI_WIDTH: $PIN_ALIGN_ROI_WIDTH" 1>&2
+#     fi
+#     if [ "xx${PIN_ALIGN_ROI_HEIGHT}" != "xx" ]; then
+#         echo "PIN_ALIGN_ROI_HEIGHT: $PIN_ALIGN_ROI_HEIGHT" 1>&2
+#     fi
+#     if [ "xx${X_CENTER}" == "xx" ]; then
+#         echo "X_CENTER: $X_CENTER" 1>&2
+#     fi
+#     if [ "xx${Y_CENTER}" == "xx" ]; then
+#         echo "Y_CENTER: $Y_CENTER" 1>&2
+#     fi
+# fi
---
+++ bash_pin_update/pin_align_prep_amx.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+#  pin_align_prep.sh -- prepare a pin alignment image for
+#                       comparison between 0 and 90 degree images
+#                       H. J. Bernstein, 3 Jan 2019
+#                       rev 16 Jan 2019
+#
+#  Version 2.0 - 15 Jun 2020
+
+if [ "${1}xx" == "--help" ]; then
+    echo "pin_align_prep.sh image_in image_out [base_image_out [sub_base_image_out]]"
+    echo "        prepare a pin alignment image, image_in, for"
+    echo "        comparison between 0 and 90 degree images"
+    echo "        writing the resulting image to image_out"
+    echo "        and optionally the 50 pixel strip in base_image_out"
+    echo "        and optionally the 80 pixel strip in sub_base_image_out"
+    echo "        assuming a 1280x1024 image and imgagemagick convert"
+    echo "        in the PATH"
+    echo "	 $2 pin tip, $3 is base, $4 is sub_base"
+    exit
+fi
+. $PIN_ALIGN_ROOT/pin_align_config_amx.sh
+
+# Windows     width x height (px) + horizontal offset + vertical offset from top left corner
+convert ${tmp_dir}/$1 -crop $PIN_ALIGN_PIN_TIP_WINDOW -canny 2x1 -negate -colorspace Gray -morphology Erode Octagon:1 -morphology Dilate Octagon:1 $2
+if [ "${3}xx" != "xx" ]; then
+    convert ${tmp_dir}/$1 -crop $PIN_ALIGN_BASE_WINDOW -canny 2x1 -negate -colorspace Gray -morphology Erode Octagon:1 -morphology Dilate Octagon:1 $3
+    if [ "${4}xx" != "xx" ]; then
+        convert ${tmp_dir}/$1 -crop $PIN_ALIGN_SUB_BASE_WINDOW -canny 2x1 -negate -colorspace Gray -morphology Erode Octagon:1 -morphology Dilate Octagon:1 $4
+    fi
+fi
+
