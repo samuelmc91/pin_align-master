b79d2417ee3b3f21493b0eb70923737bd8690f44
samuelmc91 <60661531+samuelmc91@users.noreply.github.com>
2022-04-04 12:32:00 -0400

    added default pixel output

--- pin_align_py/pin_align_config.sh
+++ pin_align_py/pin_align_config.sh
@@ -2,64 +2,64 @@
 
 ###########################################################
 
-DEFAULT_PIXELS_PER_MM="22"
-PIN_X1_OFFSET=$((15))
+DEFAULT_PIXELS_PER_MM="20"
+PIN_X1_OFFSET=$((5))
 
 export X_POS=1
 export Y_POS=1
 export Z_POS
 
 
-X_CENTER=$((402))
+X_CENTER=$((471))
 
-Y_CENTER=$((484))
+Y_CENTER=$((432))
 DEFAULT_HEIGHT=$((250))
-DEFAULT_WIDTH=$((323))
+DEFAULT_WIDTH=$((400))
 #########################################################
 
-BIG_BOX_X1=$((343))
-BIG_BOX_X2=$((666))
+BIG_BOX_X1=$((426))
+BIG_BOX_X2=$((826))
 
-BIG_BOX_Y1=$((359))
-BIG_BOX_Y2=$((609))
+BIG_BOX_Y1=$((307))
+BIG_BOX_Y2=$((557))
 
 PIN_ALIGN_ROI_WIDTH=$(((${BIG_BOX_X2}-${BIG_BOX_X1}) / 3))
 PIN_ALIGN_ROI_HEIGHT=$((${BIG_BOX_Y2}-${BIG_BOX_Y1}))
 
 ########################## pin,body,base ##########################
 
-DEFAULT_ROI_Y1=$((359))
+DEFAULT_ROI_Y1=$((307))
 
-PIN_TIP_X1=$((343))
+PIN_TIP_X1=$((426))
 
 PIN_TIP="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_TIP_X1}+${DEFAULT_ROI_Y1}"
 
-PIN_BODY_X1=$((450))
+PIN_BODY_X1=$((559))
 
 PIN_BODY="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_BODY_X1}+${DEFAULT_ROI_Y1}"
 
-PIN_BASE_X1=$((557))
+PIN_BASE_X1=$((692))
 
 PIN_BASE="${PIN_ALIGN_ROI_WIDTH}x${PIN_ALIGN_ROI_HEIGHT}+${PIN_BASE_X1}+${DEFAULT_ROI_Y1}"
 
 ########################## Tilt check  parameters ##########################
 
-TILT_CHECK_X1=$((616))
-TILT_CHECK_X2=$((666))
+TILT_CHECK_X1=$((776))
+TILT_CHECK_X2=$((826))
 
 TILT_CHECK_ROI_WIDTH=$((${TILT_CHECK_X2}-${TILT_CHECK_X1}))
 
 # Tilt check top
-TILT_CHECK_TOP_Y1=$((374))
-TILT_CHECK_TOP_Y2=$((434))
+TILT_CHECK_TOP_Y1=$((322))
+TILT_CHECK_TOP_Y2=$((382))
 
 TILT_CHECK_TOP_HEIGHT=$((${TILT_CHECK_TOP_Y2}-${TILT_CHECK_TOP_Y1}))
 
 TILT_CHECK_TOP="${TILT_CHECK_ROI_WIDTH}x${TILT_CHECK_TOP_HEIGHT}+$((${TILT_CHECK_X1} - ${PIN_BASE_X1}))+$((${TILT_CHECK_TOP_Y1} - ${DEFAULT_ROI_Y1}))"
 
 # Tilt check bottom
-TILT_CHECK_BOTTOM_Y1=$((534))
-TILT_CHECK_BOTTOM_Y2=$((594))
+TILT_CHECK_BOTTOM_Y1=$((482))
+TILT_CHECK_BOTTOM_Y2=$((542))
 
 TILT_CHECK_BOTTOM_HEIGHT=$((${TILT_CHECK_BOTTOM_Y2}-${TILT_CHECK_BOTTOM_Y1}))
 
@@ -71,15 +71,15 @@ PIN_CHECK_ROI_WIDTH=${PIN_ALIGN_ROI_WIDTH}
 
 PIN_CHECK_X1=${PIN_BODY_X1}
 
-PIN_CHECK_TOP_Y1=$((359))
-PIN_CHECK_TOP_Y2=$((434))
+PIN_CHECK_TOP_Y1=$((307))
+PIN_CHECK_TOP_Y2=$((382))
 
 PIN_CHECK_TOP_HEIGHT=$((${PIN_CHECK_TOP_Y2}-${PIN_CHECK_TOP_Y1}))
 
 PIN_CHECK_TOP="${PIN_CHECK_ROI_WIDTH}x${PIN_CHECK_TOP_HEIGHT}+0+0"
 
-PIN_CHECK_BOTTOM_Y1=$((534))
-PIN_CHECK_BOTTOM_Y2=$((609))
+PIN_CHECK_BOTTOM_Y1=$((482))
+PIN_CHECK_BOTTOM_Y2=$((557))
 
 PIN_CHECK_BOTTOM_HEIGHT=$((${PIN_CHECK_BOTTOM_Y2}-${PIN_CHECK_BOTTOM_Y1}))
 
@@ -101,7 +101,7 @@ MAX_Z=$((2))
 # ##############################################################
 
 if [ "xx${PIN_ALIGN_PIXELS_PER_MM}" == "xx" ]; then
-    PIN_ALIGN_PIXELS_PER_MM=${PIN_ALIGN_DEFAULT_PIXELS_PER_MM}
+    PIN_ALIGN_PIXELS_PER_MM=${DEFAULT_PIXELS_PER_MM}
 fi
 echo "PIN_ALIGN_PIXELS_PER_MM = ${PIN_ALIGN_PIXELS_PER_MM}"
 #################################################### EOF ####################################################
